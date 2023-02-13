eb9a3f81fb34ad0b521cc2186bb6a769669f32ac
naman9548 <125255764+naman9548@users.noreply.github.com>
2023-02-13 15:30:36 +0530

    Update w1q2.c

--- LAB/w1q2.c
+++ LAB/w1q2.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 int main()
 {
-    int array[6]={3,5,9,13,16,19},low=0,high=5,i=1;
-    int x=16;
+    int array[50],low=0,high,c=1,x;
+    printf("Enter size of array:"); 
+    scanf("%d",&high); 
+    printf("Enter elements of array:"); 
+    for(int i=0;i<high;i++) 
+    {
+        scanf("%d",&array[i]);
+    }
+    printf("Enter any element to be searched:"); 
+    scanf("%d",&x); 
+    high=high-1;
     while (low <= high) 
     {
         int mid=low+(high-low)/2;
         if (array[mid] == x)
-            printf("Element found at index %d\nComparision= %d",mid,i);
+            printf("Element found at index %d\nComparision= %d",mid,c);
         if (array[mid] < x)
         {
             low = mid + 1;
-            i++;
+            c++;
         }
         else
         {
             high = mid - 1;
-            i++;
+            c++;
         }
     }
     return 0;
 }
+
 
