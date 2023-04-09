6734d7675f11a166f83b35d54b8bb00c80e0a231
naman9548 <125255764+naman9548@users.noreply.github.com>
2023-04-09 14:43:32 +0530

    Update w1q1.c

--- LAB/w1q1.c
+++ LAB/w1q1.c
@@ -1,28 +1,30 @@
 #include <stdio.h> 
 int main()
 {
-    int n,i,arr[50],key,f=0; 
-    printf("Enter size of array:"); 
-    scanf("%d",&n); 
-    printf("Enter elements of array:"); 
-    for(i=0;i<n;i++) 
-    {
-        scanf("%d",&arr[i]);
-    }
-    printf("Enter any element to be searched:"); 
-    scanf("%d",&key); 
-    for(i=0;i<n;i++) 
-    {
-        if(key== arr[i]) 
+    int n,i,arr[50],key,f=0,t; 
+    scanf("%d",&t);
+    while(t>0){
+        scanf("%d",&n); 
+        for(i=0;i<n;i++) 
         {
-        f=1; 
-        break;
+            scanf("%d",&arr[i]);
         }
+        scanf("%d",&key); 
+        for(i=0;i<n;i++) 
+        {
+            if(key== arr[i]) 
+            {
+            f=1; 
+            break;
+            }
+        }
+        if(f==1)
+            printf("Present "); 
+        else 
+            printf("Not present "); 
+        printf("%d",i+1);
+        t--;
     }
-    if(f==1)
-        printf("Present\n"); 
-    else 
-        printf("Not present\n"); 
-    printf("%d",i+1); 
+     
     return 0;
 }
